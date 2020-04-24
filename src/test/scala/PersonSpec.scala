31cb6b332372862cfc587903c50c6f1ae0e10db3
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2020-04-24 12:59:41 +0200

    Added person creation with validation of name and age
    Signed-off-by: Paola Reyes <paolareyes.tw@packlink.com>

---
+++ src/test/scala/PersonSpec.scala
@@ -0,0 +1,43 @@
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class PersonSpec extends AnyFunSpec with Matchers {
+  describe("A Person") {
+    it("should create valid person") {
+      val expected = Person("John Doe", 18, "johndoe@example.com")
+      val actual = Person.create("John Doe", 18, "johndoe@example.com")
+      actual shouldEqual expected
+    }
+
+    it("should create valid person 2") {
+      val expected = Person("David", 65, "david@thoughtworks.com")
+      val actual = Person.create("David", 65, "david@thoughtworks.com")
+      actual shouldEqual expected
+    }
+
+    it("should not create person when name is empty") {
+      assertThrows[InvalidNameException] {
+        Person.create("", 65, "david@thoughtworks.com")
+      }
+    }
+
+    it("should not create person when name does not start with uppercase") {
+      assertThrows[InvalidNameException] {
+        Person.create("asier", 65, "david@thoughtworks.com")
+      }
+    }
+
+    it("should not create person when age is above the maximum age") {
+      assertThrows[InvalidAgeException] {
+        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
+      }
+    }
+
+    it("should not create person when age is below the minimum age") {
+      assertThrows[InvalidAgeException] {
+        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
+      }
+    }
+  }
+}
