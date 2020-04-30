b2e43d0f61272bd607db0b15548e487ce2575f75
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2020-04-30 09:52:47 +0200

    Tests using options instead of exceptions

--- src/test/scala/option/PersonSpec.scala
+++ src/test/scala/option/PersonSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 
 class PersonSpec extends AnyFunSpec with Matchers {
-  describe("A exception.Person") {
+  describe("Create a person") {
     it("should create valid person") {
       val expected = Person("John Doe", 18, "johndoe@example.com")
       val actual = Person.createOption("John Doe", 18, "johndoe@example.com")
@@ -13,40 +13,27 @@ class PersonSpec extends AnyFunSpec with Matchers {
     }
 
     it("should not create person when name is empty") {
-      assertThrows[InvalidNameException] {
-        Person.create("", 65, "david@thoughtworks.com")
-      }
       Person.createOption("", 65, "david@thoughtworks.com") shouldEqual None
     }
 
     it("should not create person when name does not start with uppercase") {
-      assertThrows[InvalidNameException] {
-        Person.create("asier", 65, "david@thoughtworks.com")
-      }
+      Person.createOption("asier", 65, "david@thoughtworks.com") shouldEqual None
     }
 
     it("should not create person when age is above the maximum age") {
-      assertThrows[InvalidAgeException] {
-        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
-      }
+      Person.createOption("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com") shouldEqual None
     }
 
     it("should not create person when age is below the minimum age") {
-      assertThrows[InvalidAgeException] {
-        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
-      }
+      Person.createOption("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com") shouldEqual None
     }
 
     it("should not create person when email is empty") {
-      assertThrows[InvalidEmailException] {
-        Person.create("John Doe", 65, "")
-      }
+      Person.createOption("John Doe", 65, "") shouldEqual None
     }
 
     it("should not create person when email has not a @ character") {
-      assertThrows[InvalidEmailException] {
-        Person.create("John Doe", 65, "john.doeATgoogle.com")
-      }
+      Person.createOption("John Doe", 65, "john.doeATgoogle.com") shouldEqual None
     }
   }
 }
