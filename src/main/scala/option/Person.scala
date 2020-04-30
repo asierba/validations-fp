8e9fe012113ad846e209e6b30ba172ee30324aa7
Paola Reyes <paolareyes.tw@packlink.com>
2020-04-30 09:30:46 +0200

    extracting validations to functions
    Signed-off-by: Asier Barrenetxea <asierba@gmail.com>

--- src/main/scala/option/Person.scala
+++ src/main/scala/option/Person.scala
@@ -10,21 +10,30 @@ object Person {
   val MAXIMUM_AGE = 120
 
   def create(name: String, age: Int, email: String): Person = {
-    val validatedName = name match {
-      case "" => throw InvalidNameException()
-      case s if s.charAt(0) != s.toUpperCase().charAt(0) => throw InvalidNameException()
-      case _ => name
-    }
-    val validatedAge = age match {
-      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE => throw InvalidAgeException()
-      case _ => age
-    }
-    val validatedEmail = email match {
+    Person(validateName(name), validateAge(age), validateEmail(email))
+  }
+
+  private def validateEmail(email: String): String = {
+    email match {
       case "" => throw InvalidEmailException()
       case e if !e.contains('@') => throw InvalidEmailException()
       case _ => email
     }
-    Person(validatedName, validatedAge, validatedEmail)
+  }
+
+  private def validateAge(age: Int): Int = {
+    age match {
+      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE => throw InvalidAgeException()
+      case _ => age
+    }
+  }
+
+  private def validateName(name: String): String = {
+    name match {
+      case "" => throw InvalidNameException()
+      case s if s.charAt(0) != s.toUpperCase().charAt(0) => throw InvalidNameException()
+      case _ => name
+    }
   }
 }
 
