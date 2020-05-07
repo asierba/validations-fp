eea27c3f9990b5a01ba1bd96d7e5db503a47b3ac
David Moran <david.moran@thoughtworks.com>
2020-05-07 09:34:41 +0200

    Renames method createEither to be create
    Signed-off-by: Paola Reyes <paolareyes.tw@packlink.com>

--- src/test/scala/either/PersonSpec.scala
+++ src/test/scala/either/PersonSpec.scala
@@ -7,52 +7,46 @@ import org.scalatest.matchers.should.Matchers
 class PersonSpec extends AnyFunSpec with Matchers {
   describe("An Either.Person") {
     it("should create valid person") {
-      val actual = Person.createEither("John Doe", 18, "johndoe@example.com")
+      val actual = Person.create("John Doe", 18, "johndoe@example.com")
 
       actual shouldEqual Right(Person("John Doe", 18, "johndoe@example.com"))
     }
 
     it("should not create person when name is empty") {
       val person: Either[InvalidError, Person] =
-        Person.createEither("", 65, "david@thoughtworks.com")
+        Person.create("", 65, "david@thoughtworks.com")
       person shouldEqual Left(InvalidError.InvalidNameError)
     }
 
     it("should not create person when name does not start with uppercase") {
       val person: Either[InvalidError, Person] =
-        Person.createEither("asier", 65, "david@thoughtworks.com")
+        Person.create("asier", 65, "david@thoughtworks.com")
 
       person shouldEqual Left(InvalidError.InvalidNameError)
     }
 
     it("should not create person when age is above the maximum age") {
-      val person: Either[InvalidError, Person] = Person.createEither(
-        "Asier",
-        Person.MAXIMUM_AGE + 1,
-        "david@thoughtworks.com"
-      )
+      val person: Either[InvalidError, Person] =
+        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
       person shouldEqual Left(InvalidError.InvalidAgeError)
     }
 
     it("should not create person when age is below the minimum age") {
-      val person: Either[InvalidError, Person] = Person.createEither(
-        "Asier",
-        Person.MINIMUM_AGE - 1,
-        "david@thoughtworks.com"
-      )
+      val person: Either[InvalidError, Person] =
+        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
       person shouldEqual Left(InvalidError.InvalidAgeError)
     }
 
     it("should not create person when email is empty") {
       val person: Either[InvalidError, Person] =
-        Person.createEither("John Doe", 65, "")
+        Person.create("John Doe", 65, "")
 
       person shouldEqual Left(InvalidError.InvalidEmailError)
     }
 
     it("should not create person when email has not a @ character") {
       val person: Either[InvalidError, Person] =
-        Person.createEither("John Doe", 65, "john.doeATgoogle.com")
+        Person.create("John Doe", 65, "john.doeATgoogle.com")
 
       person shouldEqual Left(InvalidError.InvalidEmailError)
     }
