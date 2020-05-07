bbd764eb7e2a645675c553341666feaa15f241f0
Nelson Jumbo <njumbo@thoughtworks.com>
2020-05-07 09:18:54 +0200

    first Either created for InvalidNameError
    Signed-off-by: David Moran <david.moran@thoughtworks.com>

---
+++ src/test/scala/either/PersonSpec.scala
@@ -0,0 +1,52 @@
+package either
+
+import either.InvalidError.InvalidError
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class PersonSpec extends AnyFunSpec with Matchers {
+  describe("An Either.Person") {
+    it("should create valid person") {
+      val expected = Person("John Doe", 18, "johndoe@example.com")
+      val actual = Person.create("John Doe", 18, "johndoe@example.com")
+      actual shouldEqual expected
+    }
+
+
+    it("should not create person when name is empty") {
+      val person: Either[InvalidError, Person] = Person.createEither("", 65, "david@thoughtworks.com")
+      person shouldEqual Left(InvalidError.InvalidNameError)
+    }
+
+    it("should not create person when name does not start with uppercase") {
+      assertThrows[InvalidNameException] {
+        Person.create("asier", 65, "david@thoughtworks.com")
+      }
+    }
+
+    it("should not create person when age is above the maximum age") {
+      assertThrows[InvalidAgeException] {
+        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
+      }
+    }
+
+    it("should not create person when age is below the minimum age") {
+      assertThrows[InvalidAgeException] {
+        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
+      }
+    }
+
+    it("should not create person when email is empty") {
+      assertThrows[InvalidEmailException] {
+        Person.create("John Doe", 65, "")
+      }
+    }
+
+    it("should not create person when email has not a @ character") {
+      assertThrows[InvalidEmailException] {
+        Person.create("John Doe", 65, "john.doeATgoogle.com")
+      }
+    }
+  }
+}
