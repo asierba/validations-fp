75310f4de3ec9f31f100be173b58b392d8964c06
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2020-05-07 09:30:43 +0200

    Changed age validation to work with either
    Signed-off-by: Javier Lopez <javisan81@hotmail.com>

--- src/main/scala/either/Person.scala
+++ src/main/scala/either/Person.scala
@@ -4,39 +4,42 @@ import either.InvalidError.InvalidError
 
 object InvalidError extends Enumeration {
   type InvalidError = Value
-  val InvalidNameError: either.InvalidError.Value = Value
+  val InvalidNameError, InvalidAgeError = Value
 }
 
-case class Person(name: String, age: Int, email: String) {
-
-}
+case class Person(name: String, age: Int, email: String) {}
 
 object Person {
 
   val MINIMUM_AGE = 1
   val MAXIMUM_AGE = 120
 
-  def createEither(name: String, age: Int, email: String): Either[InvalidError, Person] = {
+  def createEither(name: String,
+                   age: Int,
+                   email: String): Either[InvalidError, Person] = {
     try {
       Right(create(name, age, email))
-    }catch {
-      case _:InvalidNameException => Left(InvalidError.InvalidNameError)
+    } catch {
+      case _: InvalidNameException => Left(InvalidError.InvalidNameError)
+      case _: InvalidAgeException  => Left(InvalidError.InvalidAgeError)
     }
   }
   def create(name: String, age: Int, email: String): Person = {
     val validatedName = name match {
       case "" => throw InvalidNameException()
-      case s if s.charAt(0) != s.toUpperCase().charAt(0) => throw InvalidNameException()
+      case s if s.charAt(0) != s.toUpperCase().charAt(0) =>
+        throw InvalidNameException()
       case _ => name
     }
     val validatedAge = age match {
-      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE => throw InvalidAgeException()
+      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE =>
+        throw InvalidAgeException()
       case _ => age
     }
     val validatedEmail = email match {
-      case "" => throw InvalidEmailException()
+      case ""                    => throw InvalidEmailException()
       case e if !e.contains('@') => throw InvalidEmailException()
-      case _ => email
+      case _                     => email
     }
     Person(validatedName, validatedAge, validatedEmail)
   }
