563b58947dc7dabb4273eeaf723cea6c9c79ed1c
Paola Reyes <paolareyes.tw@packlink.com>
2020-05-07 09:36:27 +0200

    Wraps person into Right
    Signed-off-by: Nelson Jumbo <njumbo@thoughtworks.com>

--- src/main/scala/either/Person.scala
+++ src/main/scala/either/Person.scala
@@ -18,25 +18,23 @@ object Person {
              age: Int,
              email: String): Either[InvalidError, Person] = {
     try {
-      Right({
-        val validatedName = name match {
-          case "" => throw InvalidNameException()
-          case s if s.charAt(0) != s.toUpperCase().charAt(0) =>
-            throw InvalidNameException()
-          case _ => name
-        }
-        val validatedAge = age match {
-          case i if i < MINIMUM_AGE || i > MAXIMUM_AGE =>
-            throw InvalidAgeException()
-          case _ => age
-        }
-        val validatedEmail = email match {
-          case ""                    => throw InvalidEmailException()
-          case e if !e.contains('@') => throw InvalidEmailException()
-          case _                     => email
-        }
-        Person(validatedName, validatedAge, validatedEmail)
-      })
+      val validatedName = name match {
+        case "" => throw InvalidNameException()
+        case s if s.charAt(0) != s.toUpperCase().charAt(0) =>
+          throw InvalidNameException()
+        case _ => name
+      }
+      val validatedAge = age match {
+        case i if i < MINIMUM_AGE || i > MAXIMUM_AGE =>
+          throw InvalidAgeException()
+        case _ => age
+      }
+      val validatedEmail = email match {
+        case ""                    => throw InvalidEmailException()
+        case e if !e.contains('@') => throw InvalidEmailException()
+        case _                     => email
+      }
+      Right(Person(validatedName, validatedAge, validatedEmail))
     } catch {
       case _: InvalidNameException  => Left(InvalidError.InvalidNameError)
       case _: InvalidAgeException   => Left(InvalidError.InvalidAgeError)
