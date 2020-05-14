d777792f00b8d58a0997a53e8c2a2edb4c6a2c30
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2020-05-14 09:54:43 +0200

    Uses Scala's Try to handle InvalidNameException

--- src/main/scala/trypackage/Person.scala
+++ src/main/scala/trypackage/Person.scala
@@ -30,21 +30,22 @@ object Person {
   }
 
   def createWithTry(name: String, age: Int, email: String): Try[Person] = {
-    val validatedName = name match {
-      case "" => throw InvalidNameException()
-      case s if s.charAt(0) != s.toUpperCase().charAt(0) => throw InvalidNameException()
-      case _ => name
-    }
-    val validatedAge = age match {
-      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE => throw InvalidAgeException()
-      case _ => age
-    }
-    val validatedEmail = email match {
-      case "" => throw InvalidEmailException()
-      case e if !e.contains('@') => throw InvalidEmailException()
-      case _ => email
-    }
-    Try(Person(validatedName, validatedAge, validatedEmail))
+    Try(create(name, age, email))
+//    val validatedName = name match {
+//      case "" => throw InvalidNameException()
+//      case s if s.charAt(0) != s.toUpperCase().charAt(0) => throw InvalidNameException()
+//      case _ => name
+//    }
+//    val validatedAge = age match {
+//      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE => throw InvalidAgeException()
+//      case _ => age
+//    }
+//    val validatedEmail = email match {
+//      case "" => throw InvalidEmailException()
+//      case e if !e.contains('@') => throw InvalidEmailException()
+//      case _ => email
+//    }
+//    Try(Person(validatedName, validatedAge, validatedEmail))
   }
 }
 
