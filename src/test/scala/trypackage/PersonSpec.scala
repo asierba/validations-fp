13a2b481db1df518bd85b38bc255f047a4bd6d83
Javier Lopez <javisan81@hotmail.com>
2020-05-14 10:13:31 +0200

    Use scalatest TryValues for making the exceptions assertions
    Signed-off-by: David Moran <david.moran@thoughtworks.com>

--- src/test/scala/trypackage/PersonSpec.scala
+++ src/test/scala/trypackage/PersonSpec.scala
@@ -2,8 +2,9 @@ package trypackage
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.TryValues._
 
-import scala.util.{Failure, Success}
+import scala.util.Success
 
 class PersonSpec extends AnyFunSpec with Matchers {
   describe("A try.Person") {
@@ -14,79 +15,45 @@ class PersonSpec extends AnyFunSpec with Matchers {
     }
 
     it("should not create person when name is empty") {
-      val tryPerson = Person.create("", 65, "david@thoughtworks.com")
-      tryPerson match {
-        case Success(_) => fail()
-        case Failure(exception) =>
-          exception match {
-            case _: InvalidNameException => true shouldEqual true
-            case _                       => fail()
-          }
-      }
+      Person
+        .create("", 65, "david@thoughtworks.com")
+        .failure
+        .exception shouldBe a[InvalidNameException]
     }
 
     it("should not create person when name does not start with uppercase") {
-      val tryPerson =
-        Person.create("asier", 65, "david@thoughtworks.com")
-      tryPerson match {
-        case Success(_) => fail()
-        case Failure(exception) =>
-          exception match {
-            case _: InvalidNameException => true shouldEqual true
-            case _                       => fail()
-          }
-      }
+      Person
+        .create("asier", 65, "david@thoughtworks.com")
+        .failure
+        .exception shouldBe a[InvalidNameException]
     }
 
     it("should not create person when age is above the maximum age") {
-      val tryPerson =
-        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
-      tryPerson match {
-        case Success(_) => fail()
-        case Failure(exception) =>
-          exception match {
-            case _: InvalidAgeException => true shouldEqual true
-            case _                      => fail()
-          }
-      }
+      Person
+        .create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
+        .failure
+        .exception shouldBe a[InvalidAgeException]
     }
 
     it("should not create person when age is below the minimum age") {
-      val tryPerson =
-        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
-      tryPerson match {
-        case Success(_) => fail()
-        case Failure(exception) =>
-          exception match {
-            case _: InvalidAgeException => true shouldEqual true
-            case _                      => fail()
-          }
-      }
+      Person
+        .create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
+        .failure
+        .exception shouldBe a[InvalidAgeException]
     }
 
     it("should not create person when email is empty") {
-      val tryPerson = Person.create("John Doe", 65, "")
-      tryPerson match {
-        case Success(_) => fail()
-        case Failure(exception) =>
-          exception match {
-            case _: InvalidEmailException => true shouldEqual true
-            case _                        => fail()
-          }
-      }
+      Person
+        .create("John Doe", 65, "")
+        .failure
+        .exception shouldBe a[InvalidEmailException]
     }
 
     it("should not create person when email has not a @ character") {
-      val tryPerson =
-        Person.create("John Doe", 65, "john.doeATgoogle.com")
-      tryPerson match {
-        case Success(_) => fail()
-        case Failure(exception) =>
-          exception match {
-            case _: InvalidEmailException => true shouldEqual true
-            case _                        => fail()
-          }
-      }
+      Person
+        .create("John Doe", 65, "john.doeATgoogle.com")
+        .failure
+        .exception shouldBe a[InvalidEmailException]
     }
   }
 }
