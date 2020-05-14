1caae92dc6be5c0a701dc81924f8a414af6a8a5f
Nelson Jumbo <njumbo@thoughtworks.com>
2020-05-14 10:00:51 +0200

    Validates age and email using Try
    Signed-off-by: Asier Barrenetxea <asierba@gmail.com>

--- src/test/scala/trypackage/PersonSpec.scala
+++ src/test/scala/trypackage/PersonSpec.scala
@@ -5,7 +5,6 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.util.{Failure, Success}
 
-
 class PersonSpec extends AnyFunSpec with Matchers {
   describe("A try.Person") {
     it("should create valid person") {
@@ -18,45 +17,81 @@ class PersonSpec extends AnyFunSpec with Matchers {
       val tryPerson = Person.createWithTry("", 65, "david@thoughtworks.com")
       tryPerson match {
         case Success(_) => fail()
-        case Failure(exception) => exception match {
-          case _: InvalidNameException => true shouldEqual true
-          case _ => fail()
-        }
+        case Failure(exception) =>
+          exception match {
+            case _: InvalidNameException => true shouldEqual true
+            case _                       => fail()
+          }
       }
     }
 
     it("should not create person when name does not start with uppercase") {
-      val tryPerson = Person.createWithTry("asier", 65, "david@thoughtworks.com")
+      val tryPerson =
+        Person.createWithTry("asier", 65, "david@thoughtworks.com")
       tryPerson match {
         case Success(_) => fail()
-        case Failure(exception) => exception match {
-          case _: InvalidNameException => true shouldEqual true
-          case _ => fail()
-        }
+        case Failure(exception) =>
+          exception match {
+            case _: InvalidNameException => true shouldEqual true
+            case _                       => fail()
+          }
       }
     }
 
     it("should not create person when age is above the maximum age") {
-      assertThrows[InvalidAgeException] {
-        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
+      val tryPerson = Person.createWithTry(
+        "Asier",
+        Person.MAXIMUM_AGE + 1,
+        "david@thoughtworks.com"
+      )
+      tryPerson match {
+        case Success(_) => fail()
+        case Failure(exception) =>
+          exception match {
+            case _: InvalidAgeException => true shouldEqual true
+            case _                      => fail()
+          }
       }
     }
 
     it("should not create person when age is below the minimum age") {
-      assertThrows[InvalidAgeException] {
-        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
+      val tryPerson = Person.createWithTry(
+        "Asier",
+        Person.MINIMUM_AGE - 1,
+        "david@thoughtworks.com"
+      )
+      tryPerson match {
+        case Success(_) => fail()
+        case Failure(exception) =>
+          exception match {
+            case _: InvalidAgeException => true shouldEqual true
+            case _                      => fail()
+          }
       }
     }
 
     it("should not create person when email is empty") {
-      assertThrows[InvalidEmailException] {
-        Person.create("John Doe", 65, "")
+      val tryPerson = Person.createWithTry("John Doe", 65, "")
+      tryPerson match {
+        case Success(_) => fail()
+        case Failure(exception) =>
+          exception match {
+            case _: InvalidEmailException => true shouldEqual true
+            case _                        => fail()
+          }
       }
     }
 
     it("should not create person when email has not a @ character") {
-      assertThrows[InvalidEmailException] {
-        Person.create("John Doe", 65, "john.doeATgoogle.com")
+      val tryPerson =
+        Person.createWithTry("John Doe", 65, "john.doeATgoogle.com")
+      tryPerson match {
+        case Success(_) => fail()
+        case Failure(exception) =>
+          exception match {
+            case _: InvalidEmailException => true shouldEqual true
+            case _                        => fail()
+          }
       }
     }
   }
