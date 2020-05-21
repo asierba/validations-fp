3686d531a0c9f66f3408c0571e1318d7f7927fab
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2020-05-21 10:06:10 +0200

    deletes validateWithCats method to use create instead
    Signed-off-by: Paola Reyes <paolareyes.tw@packlink.com>

--- src/main/scala/validated/Person.scala
+++ src/main/scala/validated/Person.scala
@@ -16,46 +16,33 @@ object Person {
   val MINIMUM_AGE = 1
   val MAXIMUM_AGE = 120
 
-
-  def createWithCats(name: String,
-                     age: Int,
-                     email: String): Validated[InvalidError, Person] = {
-
-    val personWithEither: Either[InvalidError, Person] = create(name, age, email)
-    personWithEither match {
-      case Right(value) => Valid(value)
-      case Left(value) => Invalid(value)
-    }
-  }
-
-
   def create(name: String,
              age: Int,
-             email: String): Either[InvalidError, Person] = {
+             email: String): Validated[InvalidError, Person] = {
 
-    val validatedName = name match {
-      case "" => Left(InvalidError.InvalidNameError)
-      case s if s.charAt(0) != s.toUpperCase().charAt(0) =>
-        Left(InvalidError.InvalidNameError)
-      case _ => Right(name)
-    }
+    val validatedName: Validated[validated.InvalidError.Value, String] =
+      name match {
+        case "" => Invalid(InvalidError.InvalidNameError)
+        case s if s.charAt(0) != s.toUpperCase().charAt(0) =>
+          Invalid(InvalidError.InvalidNameError)
+        case _ => Valid(name)
+      }
     val validatedAge = age match {
       case i if i < MINIMUM_AGE || i > MAXIMUM_AGE =>
-        Left(InvalidError.InvalidAgeError)
-      case _ => Right(age)
+        Invalid(InvalidError.InvalidAgeError)
+      case _ => Valid(age)
     }
     val validatedEmail = email match {
-      case "" => Left(InvalidError.InvalidEmailError)
-      case e if !e.contains('@') => Left(InvalidError.InvalidEmailError)
-      case _ => Right(email)
+      case ""                    => Invalid(InvalidError.InvalidEmailError)
+      case e if !e.contains('@') => Invalid(InvalidError.InvalidEmailError)
+      case _                     => Valid(email)
     }
 
-    for {
-      name <- validatedName
-      age <- validatedAge
-      email <- validatedEmail
-    } yield Person(name, age, email)
-
+    validatedName.andThen(
+      name =>
+        validatedAge
+          .andThen(age => validatedEmail.map(email => Person(name, age, email)))
+    )
   }
 }
 
