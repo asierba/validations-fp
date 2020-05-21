6a3a094b789c0f25aca6e262eb9c8b6fec314967
Asier Barrenetxea <asierba@gmail.com>
2020-05-21 09:34:34 +0200

    Created person as validated
    Signed-off-by: Paola Reyes <paolareyes.tw@packlink.com>

---
+++ src/main/scala/validated/Person.scala
@@ -0,0 +1,61 @@
+package validated
+
+import cats.data.Validated
+import cats.data.Validated.Valid
+import validated.InvalidError.InvalidError
+
+object InvalidError extends Enumeration {
+  type InvalidError = Value
+  val InvalidNameError, InvalidAgeError, InvalidEmailError = Value
+}
+
+case class Person(name: String, age: Int, email: String) {}
+
+object Person {
+
+  val MINIMUM_AGE = 1
+  val MAXIMUM_AGE = 120
+
+
+  def createWithCats(name: String,
+                     age: Int,
+                     email: String): Validated[Nothing, Person] = {
+    Valid(Person(name, age, email))
+  }
+
+
+  def create(name: String,
+             age: Int,
+             email: String): Either[InvalidError, Person] = {
+
+    val validatedName = name match {
+      case "" => Left(InvalidError.InvalidNameError)
+      case s if s.charAt(0) != s.toUpperCase().charAt(0) =>
+        Left(InvalidError.InvalidNameError)
+      case _ => Right(name)
+    }
+    val validatedAge = age match {
+      case i if i < MINIMUM_AGE || i > MAXIMUM_AGE =>
+        Left(InvalidError.InvalidAgeError)
+      case _ => Right(age)
+    }
+    val validatedEmail = email match {
+      case "" => Left(InvalidError.InvalidEmailError)
+      case e if !e.contains('@') => Left(InvalidError.InvalidEmailError)
+      case _ => Right(email)
+    }
+
+    for {
+      name <- validatedName
+      age <- validatedAge
+      email <- validatedEmail
+    } yield Person(name, age, email)
+
+  }
+}
+
+case class InvalidNameException() extends Exception
+
+case class InvalidAgeException() extends Exception
+
+case class InvalidEmailException() extends Exception
---
+++ src/test/scala/validated/PersonSpec.scala
@@ -0,0 +1,60 @@
+package validated
+
+import cats.data.Validated.Valid
+import validated.InvalidError.InvalidError
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import cats.implicits._
+
+class PersonSpec extends AnyFunSpec with Matchers {
+  describe("An Either.Person") {
+
+    it("should create valid person with cats") {
+      val actual = Person.createWithCats("John Doe", 18, "johndoe@example.com")
+
+      actual shouldEqual Valid(Person("John Doe", 18, "johndoe@example.com"))
+    }
+
+    
+
+
+    it("should not create person when name is empty") {
+      val person: Either[InvalidError, Person] =
+        Person.create("", 65, "david@thoughtworks.com")
+      person shouldEqual Left(InvalidError.InvalidNameError)
+    }
+
+    it("should not create person when name does not start with uppercase") {
+      val person: Either[InvalidError, Person] =
+        Person.create("asier", 65, "david@thoughtworks.com")
+
+      person shouldEqual Left(InvalidError.InvalidNameError)
+    }
+
+    it("should not create person when age is above the maximum age") {
+      val person: Either[InvalidError, Person] =
+        Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
+      person shouldEqual Left(InvalidError.InvalidAgeError)
+    }
+
+    it("should not create person when age is below the minimum age") {
+      val person: Either[InvalidError, Person] =
+        Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
+      person shouldEqual Left(InvalidError.InvalidAgeError)
+    }
+
+    it("should not create person when email is empty") {
+      val person: Either[InvalidError, Person] =
+        Person.create("John Doe", 65, "")
+
+      person shouldEqual Left(InvalidError.InvalidEmailError)
+    }
+
+    it("should not create person when email has not a @ character") {
+      val person: Either[InvalidError, Person] =
+        Person.create("John Doe", 65, "john.doeATgoogle.com")
+
+      person shouldEqual Left(InvalidError.InvalidEmailError)
+    }
+  }
+}
