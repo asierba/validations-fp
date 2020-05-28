6de620c8627689837d9cee48bba94c344b797462
Asier Barrenetxea <asierba@gmail.com>
2020-05-28 09:57:29 +0200

    Uses validated nec to handle single errors

--- src/test/scala/validated/PersonSpec.scala
+++ src/test/scala/validated/PersonSpec.scala
@@ -1,13 +1,13 @@
 package validated
 
-import cats.data.{Chain, Validated, ValidatedNec}
 import cats.data.Validated.{Invalid, Valid}
-import validated.InvalidError.InvalidError
+import cats.data.{Chain, ValidatedNec}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import validated.InvalidError.InvalidError
 
 class PersonSpec extends AnyFunSpec with Matchers {
-  describe("An Either.Person") {
+  describe("A validated.Person") {
 
     it("should create valid person with cats") {
       val actual = Person.create("John Doe", 18, "johndoe@example.com")
@@ -16,48 +16,48 @@ class PersonSpec extends AnyFunSpec with Matchers {
     }
 
     it("should not create person with cats when name is empty") {
-      val person: Validated[InvalidError, Person] =
+      val person: ValidatedNec[InvalidError, Person] =
         Person.create("", 65, "david@thoughtworks.com")
-      person shouldEqual Invalid(InvalidError.InvalidNameError)
+      person shouldEqual Invalid(Chain(InvalidError.InvalidNameError))
     }
 
     it("should not create person when name does not start with uppercase") {
-      val person: Validated[InvalidError, Person] =
+      val person: ValidatedNec[InvalidError, Person] =
         Person.create("asier", 65, "david@thoughtworks.com")
 
-      person shouldEqual Invalid(InvalidError.InvalidNameError)
+      person shouldEqual Invalid(Chain(InvalidError.InvalidNameError))
     }
 
     it("should not create person when age is above the maximum age") {
-      val person: Validated[InvalidError, Person] =
+      val person: ValidatedNec[InvalidError, Person] =
         Person.create("Asier", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
-      person shouldEqual Invalid(InvalidError.InvalidAgeError)
+      person shouldEqual Invalid(Chain(InvalidError.InvalidAgeError))
     }
 
     it("should not create person when age is below the minimum age") {
-      val person: Validated[InvalidError, Person] =
+      val person: ValidatedNec[InvalidError, Person] =
         Person.create("Asier", Person.MINIMUM_AGE - 1, "david@thoughtworks.com")
-      person shouldEqual Invalid(InvalidError.InvalidAgeError)
+      person shouldEqual Invalid(Chain(InvalidError.InvalidAgeError))
     }
 
     it("should not create person when email is empty") {
-      val person: Validated[InvalidError, Person] =
+      val person: ValidatedNec[InvalidError, Person] =
         Person.create("John Doe", 65, "")
 
-      person shouldEqual Invalid(InvalidError.InvalidEmailError)
+      person shouldEqual Invalid(Chain(InvalidError.InvalidEmailError))
     }
 
     it("should not create person when email has not a @ character") {
-      val person: Validated[InvalidError, Person] =
+      val person: ValidatedNec[InvalidError, Person] =
         Person.create("John Doe", 65, "john.doeATgoogle.com")
 
-      person shouldEqual Invalid(InvalidError.InvalidEmailError)
+      person shouldEqual Invalid(Chain(InvalidError.InvalidEmailError))
     }
 
 //    it("should concatenate multiple errors") {
 //      val person: ValidatedNec[InvalidError, Person] =
 //        Person.createWithMultipleErrors("", Person.MAXIMUM_AGE + 1, "david@thoughtworks.com")
-//      person shouldEqual Invalid(Chain(InvalidError.InvalidNameError, InvalidError.InvalidNameError))
+//      person shouldEqual Invalid(Chain(InvalidError.InvalidNameError, InvalidError.InvalidAgeError))
 //    }
   }
 }
